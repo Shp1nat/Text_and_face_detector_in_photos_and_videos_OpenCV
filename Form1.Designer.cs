474d3fa80749c06bae5ad7cef43f3125f54319ae
Shp1nat <stac987653210@mail.ru>
2024-03-29 04:14:31 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace _5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(59, 78);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(400, 400);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(993, 78);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(400, 400);
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 46);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Загрузить изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(639, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 46);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Опознать объекты";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(639, 165);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 46);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Лицо";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(599, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(202, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(639, 217);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 46);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Загрузить видео";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(639, 269);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(115, 46);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Старт";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(639, 321);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(115, 46);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Пауза";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1476, 640);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Emgu.CV.Util;
+using Emgu.CV.OCR;
+using System.Text.RegularExpressions;
+using Emgu.CV.UI;
+
+namespace _5
+{
+    public partial class Form1 : Form
+    {
+        private Image<Bgr, byte> sourceImage;
+        private VideoCapture capture;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "Изображения (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    sourceImage = new Image<Bgr, byte>(ofd.FileName);
+                }
+                imageBox1.Image = sourceImage.Resize(400, 400, Inter.Linear);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var grayImage = advancedImageInteraction.grayscale(sourceImage.Clone());
+            var contours = advancedImageInteraction.contoursFind(grayImage);
+            var contoursImage = advancedImageInteraction.contoursDraw(sourceImage, contours);
+            var ocrFromContours = advancedImageInteraction.ocr(sourceImage, contours);
+            textBox1.Text = ocrFromContours;
+            imageBox2.Image = contoursImage.Resize(400, 400, Inter.Linear);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var facesImage = advancedImageInteraction.detectFaces(sourceImage);
+            imageBox2.Image = facesImage.Resize(400, 400, Inter.Linear);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                capture = new VideoCapture(openFileDialog.FileName);
+            }
+            capture.ImageGrabbed += ProcessFrame;
+        }
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            try
+            {
+                var frame = new Mat();
+                capture.Retrieve(frame);
+                Image<Bgr, byte> frameImage = frame.ToImage<Bgr, byte>().Resize(400, 400, Inter.Linear);
+                imageBox2.Image = advancedImageInteraction.detectFaces(frameImage);
+                Thread.Sleep((int)capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps));
+            }
+            catch (Exception)
+            {
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            capture.Start();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            capture.Pause();
+        }
+    }
+    public partial class advancedImageInteraction
+    {
+        public static Image<Gray, byte> grayscale(Image<Bgr, byte> _sourceImage)
+        {
+            var grayImage = _sourceImage.Convert<Gray, byte>();
+            grayImage._ThresholdBinaryInv(new Gray(128), new Gray(255));
+            grayImage._Dilate(5);
+            return grayImage;
+        }
+        public static VectorOfVectorOfPoint contoursFind(Image<Gray, byte> grayImg)
+        {
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(grayImg, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+            return contours;
+        }
+        public static Image<Bgr, byte> contoursDraw(Image<Bgr, byte> _sourceImage, VectorOfVectorOfPoint contours)
+        {
+            var dstImg = _sourceImage.Copy();
+            for (int i = 0; i < contours.Size; i++)
+            {
+                if (CvInvoke.ContourArea(contours[i], false) > 50)
+                {
+                    Rectangle rect = CvInvoke.BoundingRectangle(contours[i]);
+                    dstImg.Draw(rect, new Bgr(Color.Blue), 1);
+                }
+            }
+            return dstImg;
+        }
+        public static String ocr(Image<Bgr, byte> _sourceImage, VectorOfVectorOfPoint contours)
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            var dstImg = _sourceImage.Copy();
+            for (int i = 0; i < contours.Size; i++)
+            {
+                if (CvInvoke.ContourArea(contours[i], false) > 50)
+                {
+                    Rectangle rect = CvInvoke.BoundingRectangle(contours[i]);
+                    _sourceImage.ROI = rect;
+                    Image<Bgr, byte> roiImg;
+                    roiImg = _sourceImage.Clone();
+                    _sourceImage.ROI = System.Drawing.Rectangle.Empty;
+                    Tesseract _ocr = new Tesseract("D:\\tessdata-main", "eng",OcrEngineMode.TesseractLstmCombined);
+                    _ocr.SetImage(roiImg);
+                    _ocr.Recognize();
+                    Tesseract.Character[] words = _ocr.GetCharacters();
+                    for (; i < words.Length; i++)
+                    {
+                        strBuilder.Append(words[i].Text);
+                    }
+                }
+            }
+            return strBuilder.ToString();
+        }
+        public static Image<Bgr, byte> detectFaces(Image<Bgr, byte> _sourceImage)
+        {
+            Image<Bgr, byte> dstImg = _sourceImage.Clone();
+            List<Rectangle> faces = new List<Rectangle>();
+            using (CascadeClassifier face = new CascadeClassifier("D:\\haarcascades\\haarcascade_frontalface_default.xml"))
+            {
+                using (Mat ugray = new Mat())
+                {
+                    CvInvoke.CvtColor(_sourceImage, ugray,Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                    Rectangle[] facesDetected = face.DetectMultiScale(ugray, 1.1, 10, new Size(20, 20));
+                    faces.AddRange(facesDetected);
+                }
+            }
+            foreach (Rectangle rect in faces) dstImg.Draw(rect, new Bgr(Color.AliceBlue), 3);
+            return dstImg;
+        }
+
+    }
+}
